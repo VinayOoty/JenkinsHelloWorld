a0b64adfcab7122d605ef0bbf512de1bbc01948e
vinayooty <ge.vinay08@gmail.com>
2023-08-14 17:16:12 +0100

    Added new greeting message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      System.out.println("----***** Welcome to Jenkins Training - v1.2 *****----");
+      System.out.println("----***** Hello There, Welcome to Jenkins Training - v1.2 *****----");
    }
 }
